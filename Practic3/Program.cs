c466b0b5606ada7c3a00ad9cee9553f4d93a2e93
GRL7559 <martinovnikita2006@gmail.com>
2023-10-17 21:30:09 +0300

    New practic

---
+++ Practic3/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Practic_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Функциональные клавиши : Q,W,E,R,T,Y,U,I,O,P,[,],| \nДля выбора октавы нажмите 1 / 2 / 3 \nДля выхода из программы нажмите Escape");
+            bool stop=true;
+            ConsoleKey choiseR = ConsoleKey.D1;
+            while (stop==true)
+            {
+                ConsoleKeyInfo Tale = Console.ReadKey(true);
+                switch (Tale.Key)
+                {
+                    case ConsoleKey.Q:
+                        Beep(0, choiseR);
+                        break;
+                    case ConsoleKey.W:
+                        Beep(1, choiseR);
+                        break;
+                    case ConsoleKey.E :
+                        Beep(2, choiseR);
+                        break;
+                    case ConsoleKey.R :
+                        Beep(3, choiseR);
+                        break;
+                    case ConsoleKey.T :
+                        Beep(4, choiseR);
+                        break;
+                    case ConsoleKey.Y :
+                        Beep(5, choiseR);
+                        break;
+                    case ConsoleKey.U :
+                        Beep(6, choiseR);
+                        break;
+                    case ConsoleKey.I :
+                        Beep(7, choiseR);
+                        break;
+                    case ConsoleKey.O :
+                        Beep(8, choiseR);
+                        break;
+                    case ConsoleKey.P :
+                        Beep(9, choiseR);
+                        break;
+                    case ConsoleKey.Oem3 :
+                        Beep(10, choiseR);
+                        break;
+                    case ConsoleKey.Oem4 :
+                        Beep(11, choiseR);
+                        break;
+                    case ConsoleKey.Escape:
+                        stop=false;
+                        break;
+                    default:
+                        choiseR = Tale.Key;
+                        break;
+
+                }
+
+            }
+
+        }
+        static void Beep(int a , ConsoleKey choise)
+        {
+            int[] Play = Octave(choise);
+            Console.Beep(Play[a], 200);
+        }
+        static int[] Octave(ConsoleKey oct)
+        {
+            int[] octave = new int[12];
+            switch (oct)
+            {
+                case ConsoleKey.D1:
+                    octave = new int[] { 261 , 277 , 293 ,311 ,329 ,349 ,369 ,392 ,415 ,440 ,466 ,493 };
+                    break;
+                case ConsoleKey.D2:
+                    octave = new int[] { 523 ,554 ,587 ,622 ,659 ,698 ,739 ,784 ,830 ,880 ,932 ,987 };
+                    break;
+                case ConsoleKey.D3:
+                    octave = new int[] { 1046 ,1108 , 1244 ,1318 ,1396 ,1480 ,1568 ,1661 ,1720 ,1864 ,1975 };
+                    break;
+            }
+            return octave;
+        }
+    }
+}
---
+++ Practic4/Program.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.VisualBasic;
+using System.Data;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class NotesList
+    {
+        private Dictionary<DateTime, List<Note>> DateList;
+        private int currentNoteIndex;
+        private DateTime currentDate;
+        public NotesList()
+        {
+            DateList = new Dictionary<DateTime, List<Note>>();
+            currentDate = DateTime.Today;
+        }
+        public void NoteDate(Note note, DateTime date)
+        {
+            if (DateList.ContainsKey(date))
+            {
+                DateList[date].Add(note);
+            }
+            else
+            {
+                List<Note> notes = new() {note};
+                DateList.Add(date, notes);
+            }
+        }
+        public void CreateNote()
+        {
+            Console.Clear();
+            Console.WriteLine("Создание новой заметки");
+            Console.Write("Название: ");
+            string title = Console.ReadLine();
+            Console.Write("Описание: ");
+            string content = Console.ReadLine();
+            Console.Write("Дата выполнения (в формате ДД.ММ.ГГ): ");
+            bool datatest = false;
+            DateTime duedate_date = default;
+            while (datatest == false)
+            {
+                string duedate = Console.ReadLine();
+                try
+                {
+                    duedate_date = DateTime.ParseExact(duedate, "dd.MM.yy", CultureInfo.InvariantCulture);
+                    datatest = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введена некорректная дата");
+                    Console.Write("Дата выполнения (в формате ДД.ММ.ГГ): ");
+                }
+            }
+            Note newNote = new Note { Title = title, Content = content, Date = currentDate, DueDate = duedate_date };
+            NoteDate(newNote, currentDate);
+            Console.WriteLine("Новая заметка добавлена.");
+            do
+            {
+                ConsoleKeyInfo keyexit = Console.ReadKey(true);
+                if (keyexit.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            } while (true);
+        }
+        public void SwitchDate(bool move)
+        {
+            if (move)
+            {
+                currentDate = currentDate.AddDays(1);
+            }
+            else
+            {
+                currentDate = currentDate.AddDays(-1);
+            }
+            currentNoteIndex = 0;
+        }
+        public void Particulars()
+        {
+            Console.Clear();
+            if (DateList.ContainsKey(currentDate))
+            {
+                Note currentNote = DateList[currentDate][currentNoteIndex];
+                Console.WriteLine($"Заметка: {currentNote.Title}");
+                Console.WriteLine($"Описание: {currentNote.Content}");
+                Console.WriteLine($"Крайняя дата выполнения: {currentNote.DueDate:dd.MM.yy}");
+            }
+            else
+            {
+                return;
+            }
+            do
+            {
+                ConsoleKeyInfo keyexit = Console.ReadKey(true);
+                if (keyexit.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }while (true);
+        }
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine($"Дела на {currentDate.Date:dd.MM.yy}");
+        }
+        public void Arrows()
+        {
+            if (DateList.ContainsKey(currentDate))
+            {
+                List<Note> notes = DateList[currentDate];
+
+                for (int i = 0; i < notes.Count; i++)
+                {
+                    string notetitle = notes[i].Title;
+                    if (i == currentNoteIndex)
+                    {
+                        Console.WriteLine($"->{notetitle}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {notetitle}");
+                    }
+                }
+            }
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (DateList.ContainsKey(currentDate))
+                    {
+                        currentNoteIndex--;
+                        if (currentNoteIndex < 0)
+                        {
+                            currentNoteIndex = DateList[currentDate].Count - 1;
+                        }
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (DateList.ContainsKey(currentDate))
+                    {
+                        currentNoteIndex++;
+                        if (currentNoteIndex >= DateList[currentDate].Count)
+                        {
+                            currentNoteIndex = 0;
+                        }
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    SwitchDate(false);
+                    break;
+                case ConsoleKey.RightArrow:
+                    SwitchDate(true);
+                    break;
+                case ConsoleKey.Enter:
+                    Particulars();
+                    break;
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+                case ConsoleKey.N:
+                    CreateNote();
+                    break;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            NotesList DayList = new();
+            Note note1 = new() {Title = "Магазин", Content = "Список продуктов : молоко , хлеб , курица", Date = new DateTime(2023, 10, 16),DueDate = new DateTime(2023, 10, 18) };
+            Note note2 = new() {Title = "Полить цветы", Content = "Соблюдать правильный метод полива для каждого растения", Date = new DateTime(2023, 10, 17) ,DueDate = new DateTime(2023, 10, 20) };
+            Note note3 = new() {Title = "Сделать уборку", Content = "Сделать влажную уюорку всех помещений", Date = new DateTime(2023, 10, 18) ,DueDate = new DateTime(2023, 10, 19) };
+            Note note4 = new() {Title = "Написать практическую", Content = "Важно , внести финальные правки", Date = new DateTime(2023, 10, 20) ,DueDate = new DateTime(2023, 10, 21) };
+            Note note5 = new() {Title = "Приготовить еду на неделю", Content = "Расписать рацион и внести в бюджет расходы на продукты", Date = new DateTime(2023, 10, 19) ,DueDate = new DateTime(2023, 10, 20) };
+            DayList.NoteDate(note1, note1.Date);
+            DayList.NoteDate(note2, note2.Date);
+            DayList.NoteDate(note3, note3.Date);
+            DayList.NoteDate(note4, note4.Date);
+            DayList.NoteDate(note5, note5.Date);
+            while (true)
+            {
+                DayList.Menu();
+                DayList.Arrows();
+            }
+        }
+    }
+}
