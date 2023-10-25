bed28ab46683a520731fe3d87901bd7bbdf0fc85
GRL7559 <martinovnikita2006@gmail.com>
2023-10-26 01:01:04 +0300

    gd

---
+++ Practic6/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic6
+{
+    public class Data
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string[] Clothes { get; set; }
+    }
+}
---
+++ Practic6/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Practic6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Data> data = new();
+            Read_file();
+            
+        }
+        static void Read_file()
+        {
+
+            Console.WriteLine("Какой файл нужно прочитать? (Укажите полный путь до файла и его расширение)");
+            string path_file = Console.ReadLine();
+            if (path_file.EndsWith(".txt"))
+            {
+                string txt_file = File.ReadAllText(path_file);
+                List<Data> txt_data = JsonConvert.DeserializeObject<List<Data>>(txt_file);
+                Convert_file(txt_data);
+            }
+            else if(path_file.EndsWith(".xml"))
+            {
+                XmlSerializer xml_file = new(typeof(List<Data>));
+                using (FileStream fs = new(path_file, FileMode.Open))
+                {
+                    List<Data> Xml_data = (List<Data>)xml_file.Deserialize(fs);
+                    Convert_file(Xml_data);
+                }
+            }
+            else if (path_file.EndsWith(".json"))
+            {
+                string J_file = File.ReadAllText(path_file);
+                List<Data> json_data = JsonConvert.DeserializeObject<List<Data>>(J_file);
+                Console.WriteLine(json_data);
+                Convert_file(json_data);
+            }
+        }
+        public static void Convert_file(List<Data> data)
+        {
+            Console.WriteLine("Куда и в каком формате нужно сохранить данные ? (Укажите полный путь до файла и его расширение)");
+            string path_file = Console.ReadLine();
+            if (path_file.EndsWith(".txt"))
+            {
+                string txt_data = JsonConvert.SerializeObject(data);
+                File.WriteAllText(path_file,txt_data);
+            }
+            else if (path_file.EndsWith(".xml"))
+            {
+                XmlSerializer xml = new(typeof(List<Data>));
+                using (FileStream fs = new(path_file , FileMode.OpenOrCreate))
+                {
+                    xml.Serialize(fs,data);
+                }
+            }
+            else if (path_file.EndsWith(".json"))
+            {
+                string json_data = JsonConvert.SerializeObject(data,Formatting.Indented);
+                File.WriteAllText(path_file, json_data);
+            }
+        }
+    }
+}
