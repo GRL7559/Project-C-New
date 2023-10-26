21d12c1c1924325be9bae2103f769413d8338613
GRL7559 <martinovnikita2006@gmail.com>
2023-10-26 18:25:18 +0300

    globglogabgalab

--- Practic6/Data.cs
+++ Practic6/Data.cs
@@ -10,6 +10,18 @@ namespace Practic6
     {
         public int Age { get; set; }
         public string Name { get; set; }
-        public string[] Clothes { get; set; }
+        public string Clothes { get; set; }
+        public Data(int age, string name, string clothes)
+        {
+            Age = age;
+            Name = name;
+            Clothes = clothes;
+        }
+        public Data()
+        {
+            Age = 0;
+            Name = string.Empty;
+            Clothes = string.Empty;
+        }
     }
 }
--- Practic6/Program.cs
+++ Practic6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace Practic6
@@ -8,59 +9,116 @@ namespace Practic6
     {
         static void Main()
         {
-            List<Data> data = new();
-            Read_file();
-            
-        }
-        static void Read_file()
-        {
-
-            Console.WriteLine("Какой файл нужно прочитать? (Укажите полный путь до файла и его расширение)");
-            string path_file = Console.ReadLine();
-            if (path_file.EndsWith(".txt"))
+            List<Data> data = Read_file();
+            bool stop = true;
+            while (stop == true)
             {
-                string txt_file = File.ReadAllText(path_file);
-                List<Data> txt_data = JsonConvert.DeserializeObject<List<Data>>(txt_file);
-                Convert_file(txt_data);
-            }
-            else if(path_file.EndsWith(".xml"))
-            {
-                XmlSerializer xml_file = new(typeof(List<Data>));
-                using (FileStream fs = new(path_file, FileMode.Open))
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
                 {
-                    List<Data> Xml_data = (List<Data>)xml_file.Deserialize(fs);
-                    Convert_file(Xml_data);
+                    case ConsoleKey.Enter:
+                        Convert_file(data);
+                        stop = false;
+                        break;
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("нажмите Enter для продолжения работы с выбранным файлом или Esc для выхода из программы");
+                        break;
+
                 }
             }
-            else if (path_file.EndsWith(".json"))
+        }
+        private static List<Data> Read_file()
+        {
+            while (true)
             {
-                string J_file = File.ReadAllText(path_file);
-                List<Data> json_data = JsonConvert.DeserializeObject<List<Data>>(J_file);
-                Console.WriteLine(json_data);
-                Convert_file(json_data);
-            }
+                Console.WriteLine("Какой файл нужно прочитать? (Укажите полный путь до файла и его расширение)");
+                string path_file = Console.ReadLine();
+                if (path_file.EndsWith(".txt"))
+                {
+                    List<Data> txt_data = new();
+                    string[] txt_lines = File.ReadAllLines(path_file);
+                    for (int i = 0; i < txt_lines.Length; i += 3)
+                    {
+                        int Age = Convert.ToInt32(txt_lines[i]);
+                        string Name = txt_lines[i + 1];
+                        string Clothes = txt_lines[i + 2];
+                        Console.WriteLine($"{Age}\n{Name}\n{Clothes}");
+                        Data txt_file = new(Age, Name, Clothes);
+                        txt_data.Add(txt_file);
+                    }
+                    return txt_data;
+                }
+                else if (path_file.EndsWith(".xml"))
+                {
+                    XmlSerializer xml_file = new(typeof(List<Data>));
+                    using FileStream fs = new(path_file, FileMode.Open);
+                    List<Data> Xml_data = (List<Data>)xml_file.Deserialize(fs);
+                    for (int i = 0; i < Xml_data.Count; i++)
+                    {
+                        Console.WriteLine(Xml_data[i].Age);
+                        Console.WriteLine(Xml_data[i].Name);
+                        Console.WriteLine(Xml_data[i].Clothes);
+                    }
+                    return Xml_data;
+                }
+                else if (path_file.EndsWith(".json"))
+                {
+                    string J_file = File.ReadAllText(path_file);
+                    List<Data> json_data = JsonConvert.DeserializeObject<List<Data>>(J_file);
+                    for (int i = 0; i < json_data.Count; i++)
+                    {
+                        Console.WriteLine(json_data[i].Age);
+                        Console.WriteLine(json_data[i].Name);
+                        Console.WriteLine(json_data[i].Clothes);
+                    }
+                    return json_data;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректный путь или расширение");
+                }
+            }       
         }
         public static void Convert_file(List<Data> data)
         {
-            Console.WriteLine("Куда и в каком формате нужно сохранить данные ? (Укажите полный путь до файла и его расширение)");
-            string path_file = Console.ReadLine();
-            if (path_file.EndsWith(".txt"))
+            while (true)
             {
-                string txt_data = JsonConvert.SerializeObject(data);
-                File.WriteAllText(path_file,txt_data);
-            }
-            else if (path_file.EndsWith(".xml"))
-            {
-                XmlSerializer xml = new(typeof(List<Data>));
-                using (FileStream fs = new(path_file , FileMode.OpenOrCreate))
+                Console.WriteLine("Куда и в каком формате нужно сохранить данные ? (Укажите полный путь до файла и его расширение)");
+                string path_file = Console.ReadLine();
+
+                if (path_file.EndsWith(".txt"))
                 {
-                    xml.Serialize(fs,data);
+                    string[] txt_lines = new string[data.Count * 3];
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        txt_lines[i + i * 2] = Convert.ToString(data[i].Age);
+                        txt_lines[i + 1 + i * 2] = data[i].Name;
+                        txt_lines[i + 2 + i * 2] = data[i].Clothes;
+
+                    }
+                    File.WriteAllLines(path_file, txt_lines);
+                    return;
                 }
-            }
-            else if (path_file.EndsWith(".json"))
-            {
-                string json_data = JsonConvert.SerializeObject(data,Formatting.Indented);
-                File.WriteAllText(path_file, json_data);
+                else if (path_file.EndsWith(".xml"))
+                {
+                    XmlSerializer xml = new(typeof(List<Data>));
+                    using FileStream fs = new(path_file, FileMode.OpenOrCreate);
+                    xml.Serialize(fs, data);
+                    return;
+                }
+                else if (path_file.EndsWith(".json"))
+                {
+                    string json_data = JsonConvert.SerializeObject(data, Formatting.Indented);
+                    File.WriteAllText(path_file, json_data);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректный путь или расширение");
+                }
             }
         }
     }
